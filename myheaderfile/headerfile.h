6f4924bb7d617b80ca4f0b6ac44028c628b95c83
pawan405 <pavanjural19@gmail.com>
2025-04-29 15:46:09 +0530

    updated one

--- myheaderfile/headerfile.h
+++ myheaderfile/headerfile.h
@@ -1,37 +1,59 @@
-#ifndef headerfile_H
-#define headerfile_H
-#include<stdio.h>
-int find_index(int arr[],int size,int target);
-int sum_of_array(int arr[],int sum);
-int max_of_array(int arr[],int max);
+// #ifndef headerfile_H
+// #define headerfile_H
+// #include<stdio.h>
+// int find_index(int arr[],int size,int target);
+// int sum_of_array(int arr[],int sum);
+// int max_of_array(int arr[],int max);
 
-int find_index(int arr[], int size, int target) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
-            return i;  
-        }
-    }
-    return -1;  
-}
-
-
-int sum_of_array(int arr[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += arr[i];
-    }
-    return sum;
-}
-
-int max_of_array(int arr[], int size) {
-    int max = arr[0]; 
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    return max;
-}
+// int find_index(int arr[], int size, int target) {
+//     for (int i = 0; i < size; i++) {
+//         if (arr[i] == target) {
+//             return i;  
+//         }
+//     }
+//     return -1;  
+// }
 
 
-#endif // headerfile_H
+// int sum_of_array(int arr[], int size) {
+//     int sum = 0;
+//     for (int i = 0; i < size; i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// int max_of_array(int arr[], int size) {
+//     int max = arr[0]; 
+//     for (int i = 1; i < size; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+
+// #endif // headerfile_H
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+#include <stdio.h>
+
+// Function pointer typedefs for modularity
+typedef int (*FindIndexFunc)(int[], int, int);
+typedef int (*ArrayOpFunc)(int[], int);
+
+// Function declarations
+int find_index(int arr[], int size, int target);
+int sum_of_array(int arr[], int size);
+int max_of_array(int arr[], int size);
+
+// Struct to group function pointers
+typedef struct {
+    FindIndexFunc find_index;
+    ArrayOpFunc sum;
+    ArrayOpFunc max;
+} ArrayOperations;
+
+#endif // HEADERFILE_H
--- myheaderfile/try1.c
+++ myheaderfile/try1.c
@@ -1,3 +1,35 @@
+// #include "headerfile.h"
+
+// int main() {
+//     int n, target;
+
+//     printf("Enter number of elements: ");
+//     scanf("%d", &n);
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Enter element to find index: ");
+//     scanf("%d", &target);
+
+//     int index = find_index(arr, n, target);
+//     if (index != -1) {
+//         printf("Element found at index: %d\n", index);
+//     } else {
+//         printf("Element not found in array.\n");
+//     }
+
+//     int sum = sum_of_array(arr, n);
+//     printf("Sum of array elements: %d\n", sum);
+
+
+//     int max = max_of_array(arr, n);
+//     printf("Maximum element in array: %d\n", max);
+
+//     return 0;
+// }
 #include "headerfile.h"
 
 int main() {
@@ -14,19 +46,23 @@ int main() {
     printf("Enter element to find index: ");
     scanf("%d", &target);
 
-    int index = find_index(arr, n, target);
+    // Create and initialize the function pointer struct
+    ArrayOperations ops = {find_index, sum_of_array, max_of_array};
+
+    // Use function pointers
+    int index = ops.find_index(arr, n, target);
     if (index != -1) {
         printf("Element found at index: %d\n", index);
     } else {
         printf("Element not found in array.\n");
     }
 
-    int sum = sum_of_array(arr, n);
+    int sum = ops.sum(arr, n);
     printf("Sum of array elements: %d\n", sum);
 
-
-    int max = max_of_array(arr, n);
+    int max = ops.max(arr, n);
     printf("Maximum element in array: %d\n", max);
 
     return 0;
 }
+
